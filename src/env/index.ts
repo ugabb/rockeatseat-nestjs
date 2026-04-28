22a9807ed8af9594e5f3687051db580a1395655f
Gabriel Barros <ugabrieldev@gmail.com>
2026-04-28 19:59:13 -0300

    add simple auth config module

--- src/env/index.ts
+++ src/env/index.ts
@@ -3,6 +3,7 @@ import { z } from "zod";
 export const envSchema = z.object({
   PORT: z.coerce.number().optional().default(3333),
   DATABASE_URL: z.string(),
+  JWT_SECRET: z.string(),
 });
 
 export type Env = z.infer<typeof envSchema>;
